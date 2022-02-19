b1c17df2c714a369f8f45c032e2534ba03c37b4b
yuhueilee <ylee0026@student.monash.edu>
2022-02-19 13:46:26 +0800

    Use Navigate to direct default path to a certain path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; 
 import Repos from './components/Repos';
 import Repo from './components/Repo';
 
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <div>
         <Routes>
+          <Route path="/" element={<Navigate replace to="/users/yuhueilee/repos" />} />
           <Route path='/users/:username/repos' element={<Repos />} />
           <Route path='/users/:username/repos/:reponame' element={<Repo />} />
         </Routes>
