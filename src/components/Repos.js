a257044ad3d27c3ce9ef9df75b1b4d42a36a34aa
yuhueilee <ylee0026@student.monash.edu>
2022-02-19 14:05:25 +0800

    Use hooker to fetch the repos, display using map function

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { useParams } from "react-router-dom";
+import React, { useState } from 'react';
+import { useParams, Link } from "react-router-dom";
+import useReposSearch from './useReposSearch';
+
+import './Components.css';
 
 function Repos() {
-
+  // get the param in the route
   const { username } = useParams();
+
+  // create state for page number and default to 1
+  const [ pageNumber, setPageNumber ] = useState(1);
+
+  // get the repos using hook
+  const { 
+    loading, 
+    error, 
+    repos, 
+    hasMore 
+  } = useReposSearch(username, pageNumber);
 
   return (
     <div>
-      <h1>User {username}</h1>
+      <h2 className='font heading text-center'>{username}'s Repository List</h2>
+      {repos.map(repo => {
+        return (
+          <div>
+            <p className='font'>Repository Name: <b>{repo.name}</b></p>
+            <p className='font'>Star Count: <b>{repo.stargazers_count}</b></p>
+            <Link className='font' to={`/users/${username}/repos/${repo.name}`}>Click Me</Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
