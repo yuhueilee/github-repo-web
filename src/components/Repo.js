90e811f45e9fc73178ad02068de8ff2d0050a05a
yuhueilee <ylee0026@student.monash.edu>
2022-02-19 14:07:45 +0800

    Use hooker to fetch a single repo and display

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
+import useRepoSearch from './useRepoSearch';
 
 function Repo() {
 
   const { username, reponame } = useParams();
 
+  const { 
+    loading, 
+    error, 
+    repo
+  } = useRepoSearch(username, reponame);
+
   return (
     <div>
-      <h1>User {username} Repo {reponame}</h1>
+      <h2 className='font'>{repo?.full_name}</h2>
+      <p className='font'>Description: {repo?.description}</p>
+      <p className='font'>Start Count: {repo?.stargazers_count}</p>
+      <p className='font'>See more in <a href={repo?.html_url} target="_blank" rel="noreferrer">GitHub</a></p>
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
     </div>
   );
 }
