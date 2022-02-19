4fc09b66e6dbc01e61664dfca7fda23d830fb8c9
yuhueilee <ylee0026@student.monash.edu>
2022-02-19 14:13:02 +0800

    Create a ref variable that returns a memorized callback function to update the page number when the last node is visible and there are more repos to be loaded

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { useParams, Link } from "react-router-dom";
 import useReposSearch from './useReposSearch';
 
@@ -19,17 +19,43 @@ function Repos() {
     hasMore 
   } = useReposSearch(username, pageNumber);
 
+  const observer = useRef();
+  const lastRepoElementRef = useCallback(node => {
+    // prevent calling observer when data is still loading
+    if (loading) return;
+    // disconnect the current observer
+    if (observer.current) observer.current.disconnect();
+    // initialize a new observer and update page number if there's more to load
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1);
+      }
+    });
+    // observe the last node
+    if (node) observer.current.observe(node);
+  }, [loading, hasMore]);
+
   return (
     <div>
       <h2 className='font heading text-center'>{username}'s Repository List</h2>
-      {repos.map(repo => {
-        return (
-          <div>
-            <p className='font'>Repository Name: <b>{repo.name}</b></p>
-            <p className='font'>Star Count: <b>{repo.stargazers_count}</b></p>
-            <Link className='font' to={`/users/${username}/repos/${repo.name}`}>Click Me</Link>
-          </div>
-        );
+      {repos.map((repo, index) => {
+        if (repos.length === index + 1) {
+          return (
+            <div ref={lastRepoElementRef}>
+              <p className='font'>Repository Name: <b>{repo.name}</b></p>
+              <p className='font'>Star Count: <b>{repo.stargazers_count}</b></p>
+              <Link className='font' to={`/users/${username}/repos/${repo.name}`}>Click Me</Link>
+            </div>
+          );
+        } else {
+          return (
+            <div>
+              <p className='font'>Repository Name: <b>{repo.name}</b></p>
+              <p className='font'>Star Count: <b>{repo.stargazers_count}</b></p>
+              <Link className='font' to={`/users/${username}/repos/${repo.name}`}>Click Me</Link>
+            </div>
+          );
+        }
       })}
     </div>
   );
