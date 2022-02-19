df6f6b46c546181823a879c3c7766d4361bc7c87
yuhueilee <ylee0026@student.monash.edu>
2022-02-19 14:30:41 +0800

    Remove unused import

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useCallback } from 'react';
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useReposSearch from './useReposSearch';
 
 import {
