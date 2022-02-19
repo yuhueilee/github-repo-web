077eb50841cb58f20f9a72b439511fc70c26d873
yuhueilee <ylee0026@student.monash.edu>
2022-02-19 14:39:24 +0800

    Format documents

--- src/components/useRepoSearch.js
+++ src/components/useRepoSearch.js
@@ -1,33 +1,34 @@
-import { useState, useEffect } from 'react';
-import { Octokit } from '@octokit/core';
-
+import { useState, useEffect } from "react";
+import { Octokit } from "@octokit/core";
 
 const useRepoSearch = (username, reponame) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [repo, setRepo] = useState(null);
 
-    const [ loading, setLoading ] = useState(true);
-    const [ error, setError ] = useState(false);
-    const [ repo, setRepo ] = useState(null);
+  // fetch the repo and set the state
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
 
-    // fetch the repo and set the state
-    useEffect(() => {
-        setLoading(true);
-        setError(false);
+    const octokit = new Octokit({ auth: process.env.REACT_APP_GH });
 
-        const octokit = new Octokit({ auth: process.env.REACT_APP_GH });
+    octokit
+      .request("GET /repos/{owner}/{repo}", {
+        owner: username,
+        repo: reponame,
+      })
+      .then((res) => {
+        setRepo(res.data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+        setError(true);
+      });
+  }, [username, reponame]);
 
-        octokit.request('GET /repos/{owner}/{repo}', {
-            owner: username,
-            repo: reponame
-        }).then(res => {
-            setRepo(res.data);
-            setLoading(false);
-        }).catch(e => {
-            console.log(e);
-            setError(true);
-        });
-    }, [username, reponame]);
-
-    return { loading, error, repo };
-}
+  return { loading, error, repo };
+};
 
 export default useRepoSearch;
